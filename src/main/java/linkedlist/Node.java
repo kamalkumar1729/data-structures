a6a15d5a97bcb53cc328efedbc61b95b48556b15
Kamal Kumar <kamal7.kumar@ril.com>
2019-11-22 14:11:16 +0530

    initial commit

--- src/main/java/linkedlist/Node.java
+++ src/main/java/linkedlist/Node.java
@@ -1,7 +1,11 @@
 package linkedlist;
 
 public class Node {
+
+    private Node node;
+
     public static void main(String[] args) {
-        /*      */
+        /* linked list  */
+        new Node();
     }
 }

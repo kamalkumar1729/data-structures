6781a1af8307178e4331f37b169f225555236a16
Kamal Kumar <kamalkumar1729@gamil.com>
2021-04-20 11:10:44 +0530

    read write lock

--- src/main/java/learnjava/concurrency/ReadWriteLockTest.java
+++ src/main/java/learnjava/concurrency/ReadWriteLockTest.java
@@ -1,15 +1,49 @@
 package learnjava.concurrency;
 
-import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ReadWriteLockTest {
-    public static void main(String[] args) {
+
+    static ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    public static void main(String[] args) throws InterruptedException {
+
+        new Thread(new ReaderTask()).start();
+        new Thread(new WriterTask()).start();
+        new Thread(new ReaderTask()).start();
+        new Thread(new WriterTask()).start();
+        new Thread(new ReaderTask()).start();
+        new Thread(new WriterTask()).start();
+        new Thread(new ReaderTask()).start();
+
+    }
 
 
-        ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    static class ReaderTask implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("locked by writer : " + readWriteLock.isWriteLocked());
+            readWriteLock.readLock().lock();
+            try {
+                System.out.println("reading by " + Thread.currentThread().getName());
+            } finally {
+                readWriteLock.readLock().unlock();
+            }
+        }
+    }
 
-
-
+    static class WriterTask implements Runnable {
+        @Override
+        public void run() {
+            readWriteLock.writeLock().lock();
+            try {
+                System.out.println("writing by : " + Thread.currentThread().getName());
+                Thread.sleep(5000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                readWriteLock.writeLock().unlock();
+            }
+        }
     }
 }
